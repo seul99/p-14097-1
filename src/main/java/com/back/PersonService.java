a3decc7c22b77eace8e000281f8f80be9765e889
sia <piao516@naver.com>
2025-11-21 15:51:16 +0900

    5-1

--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -1,13 +1,18 @@
 package com.back;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PersonService {
-    private final PersonRepository personRepositoryV2;
+    private final PersonRepository personRepository;
+    public PersonService(@Qualifier("personRepositoryV2") PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     public long count() {
-        return personRepositoryV2.count();
+        return personRepository.count();
     }
 }
